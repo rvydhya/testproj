661efa167c70f61777d0286cc97642385af95d64
rvydhya <rvydhya@outlook.com>
2016-01-11 19:09:30 +0530

    Initial commit

---
+++ testproj/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testproj.Startup))]
+namespace testproj
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
